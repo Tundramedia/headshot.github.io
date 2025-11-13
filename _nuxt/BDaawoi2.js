1bc55ef93e920011eccb74296d6acf18974a5fb1
akasangre <akasangre@users.noreply.github.com>
2025-11-13 05:44:15 +0000

    deploy: Tundramedia/headshot@960c62876c33a7548c7ae8835107c16df947e294

---
+++ _nuxt/BDaawoi2.js
@@ -0,0 +1,1 @@
+import{p as s}from"./BMKicV3f.js";const t=s("/assets/img/qr.png");export{t as _};
---
+++ _nuxt/Bm0IfvcE.js
@@ -0,0 +1,1 @@
+import{p as s}from"./BMKicV3f.js";const p=s("/assets/img/logo.png");export{p as _};
